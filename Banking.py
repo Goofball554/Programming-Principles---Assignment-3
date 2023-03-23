cc02c531d2f74c7a09d011b48921097a853a0e47
Alex <123321081+Goofball554@users.noreply.github.com>
2023-03-23 11:59:46 -0400

    Setup Functions

---
+++ Banking.py
@@ -0,0 +1,63 @@
+class Program():
+    def run():
+        pass
+
+    def showMainMenu():
+        pass
+
+    def showAccountMenu():
+        pass
+
+
+class Account():
+    def __init__():
+        pass
+
+    def getAccountNumber():
+        pass
+
+    def getAccountHolderName():
+        pass
+
+    def getRateOfInterest():
+        pass
+
+    def getCurrentBalance():
+        pass
+
+    def deposit():
+        pass
+
+    def withdraw():
+        pass
+    
+    pass
+
+class SavingsAccount(Account):
+    def __init__():
+        pass
+    
+    def withdraw():
+        pass
+   
+    pass
+
+class ChecquingAccount(Account):
+    def __init__():
+        pass
+    
+    def withdraw():
+        pass
+
+    pass
+
+class Bank():
+    def searchAccount():
+        pass
+    
+    pass
+
+
+
+
+
