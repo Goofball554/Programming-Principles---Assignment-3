e970e246d8ef0b53f4109a5c8072f0f0e4e2e356
Alex <123321081+Goofball554@users.noreply.github.com>
2023-03-23 13:00:02 -0400

    Created Account Menu

--- Banking.py
+++ Banking.py
@@ -1,15 +1,65 @@
 class Program():
-    def showMainMenu():
-        print("1. Select Account")
-        print("2. Exit")
-
+    def showAccountMenu(accountNumber):
         try: 
+            print("ACCOUNT MENU")
+            print("Account Number: ", accountNumber)
+            print("1. Check Balance")
+            print("2. Deposit")
+            print("3. Withdraw")
+            print("4. Exit Account")
+            userInput2 = int(input())
+
+        except:
+            userInput2 = 0
+
+        if userInput2 == 1:
+            pass
+        
+        elif userInput2 == 2:
+            pass
+
+        elif userInput2 == 3:
+            pass
+        
+        elif userInput2 == 4:
+            Program.showMainMenu()
+
+        else:
+            print("Please enter a valid input (either 1 - 5).")
+            Program.showAccountMenu()
+        
+
+
+    def showMainMenu():
+        try: 
+            print("MAIN MENU")
+            print("1. Select Account")
+            print("2. Exit")
             userInput = int(input())
+        
         except:
             userInput = 0
 
         if userInput == 1:
-            Program.showAccountMenu()
+            print("Available Accounts:")
+            print("Account Number | Account Name")
+            print("1 | Bobby")
+            print("2 | Joe")
+            print("3 | Jasmine")
+            print("4 | Melanie")
+            print("5 | Richard")
+
+            try:
+                accountNumber = int(input())
+            
+            except:
+                print("Please enter a valid input (1 - 5).")
+
+            if accountNumber >= 1 and accountNumber <= 5:
+                Program.showAccountMenu(accountNumber)
+
+            else:
+                print("Please enter a valid input (1 - 5).")
 
         elif userInput == 2:
             pass
@@ -17,9 +67,6 @@ class Program():
         else:
             print("Please enter a valid input (either 1 or 2).")
             Program.showMainMenu()
-
-    def showAccountMenu():
-        print("We got here")
 
     def run():
         Program.showMainMenu()
@@ -71,6 +118,5 @@ class Bank():
         pass
     
     pass
-
 
 Program.run()
